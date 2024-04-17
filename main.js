0cbbf650149a35d41e0fb04bf2a1b28dfeb427f1
Paulo Cauan <104834064+PauloCauan@users.noreply.github.com>
2024-04-17 16:40:20 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
+
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
@@ -15,16 +16,15 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-let tempoAtual = new Date();
 
-//contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempos[0]);
 
@@ -33,7 +33,6 @@ function atualizaCronometro(){
         contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
-
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
@@ -60,4 +59,3 @@ function calculaTempo(tempoObjetivo) {
     }
 
 }
-
